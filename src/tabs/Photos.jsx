8db9e8c427e906e6210782181937990f58d99dfd
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-05-18 11:39:28 +0300

    add

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,9 +1,46 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from "apiService/photos";
+import { Form, Text } from "components";
+import { useEffect, useState } from "react";
 
 export const Photos = () => {
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const { photos, per_page, total_results } = await getPhotos(
+          query,
+          page
+        );
+        if (photos.length === 0) {
+          setIsEmpty(true);
+          return;
+        }
+        setImages((prevImages) => [...prevImages, ...photos]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [query, page]);
+
+  const onHandleSubmit = (value) => {
+    setQuery(value);
+  };
+
   return (
     <>
+      <Form onSubmit={onHandleSubmit} />
       <Text textAlign="center">Let`s begin search 🔎</Text>
     </>
   );
